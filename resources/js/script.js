1da69f3cdd7beb16e7f2166c24b1c7faf845b698
Franzi Grieser <franzi.grieser@gmail.com>
2020-10-11 12:03:58 +0200

    add animations

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    // sticky navigation
     $(".js--section-features").waypoint(function(direction) {
         if (direction == "down") {
             $("nav").addClass("sticky")
@@ -8,4 +9,41 @@ $(document).ready(function() {
     }, {
       offset: "61px"
     });
+
+
+    // scroll to buttons
+    $(".js--scroll-to-plan").click( function() {
+        $("html, body").animate({scrollTop: $(".js--section-plans").offset().top}, 1000);
+    });
+
+    $(".js--scroll-to-features").click( function() {
+        $("html, body").animate({scrollTop: $(".js--section-features").offset().top}, 1000);
+    });
+
+
+    // animations
+    $(".js--wp-1").waypoint(function(direction) {
+        $(".js--wp-1").addClass("animate__animated animate__fadeIn");
+      },
+      {
+        offset: "70%",
+      });
+    $(".js--wp-2").waypoint(function(direction) {
+        $(".js--wp-2").addClass("animate__animated animate__fadeInUp");
+      },
+      {
+        offset: '70%',
+      });
+    $(".js--wp-3").waypoint(function(direction) {
+        $(".js--wp-3").addClass("animate__animated animate__fadeIn");
+      },
+      {
+        offset: '60%',
+      });
+    $(".js--wp-4").waypoint(function(direction) {
+        $(".js--wp-4").addClass("animate__animated animate__pulse");
+      },
+      {
+        offset: '50%',
+      });
 })
